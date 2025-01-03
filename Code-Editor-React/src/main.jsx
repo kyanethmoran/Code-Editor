a92af01d7eca7c358bbe0fb3d0b8f3d9484d686d
moran <sharon.ky.moran@gmail.com>
2025-01-03 18:23:20 -0500

    forced darkmode

--- Code-Editor-React/src/main.jsx
+++ Code-Editor-React/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import { ChakraProvider } from '@chakra-ui/react'
+// import theme from './theme.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {localStorage.setItem('chakra-ui-color-mode', 'dark')} 
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </StrictMode>,
 )
