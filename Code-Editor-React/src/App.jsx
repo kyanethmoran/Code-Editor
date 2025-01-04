7339a775317b00cafe0213b3b479a8c98f271ca7
moran <sharon.ky.moran@gmail.com>
2025-01-03 21:20:16 -0500

    integrate monaco code editor text box

--- Code-Editor-React/src/App.jsx
+++ Code-Editor-React/src/App.jsx
@@ -1,10 +1,12 @@
-import { Box, color } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
+import CodeEditor from './components/codeEditor'
 
 function App() {
 
   return (
     <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
       Code Editor React
+      <CodeEditor />
     </Box>
   )
 }
--- Code-Editor-React/src/components/codeEditor.jsx
+++ Code-Editor-React/src/components/codeEditor.jsx
@@ -1,12 +1,26 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
+import { Box } from "@chakra-ui/react";
+import { Editor } from "@monaco-editor/react";
+import { useState } from "react";
 
-function codeEditor() {
+function CodeEditor() {
+  const [value, setValue] = useState("");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
   return (
     <Box>
-        
+      <Editor
+        height="75vh"
+        theme="vs-dark"
+        defaultLanguage="javascript"
+        defaultValue="//Type Your code Here..."
+        value={value}
+        onChange={handleChange}
+      />
     </Box>
-  )
+  );
 }
 
-export default codeEditor
+export default CodeEditor;
