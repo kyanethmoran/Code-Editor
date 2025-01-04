19da556464f149653f9dfd6d1250d8a49522b02a
moran <sharon.ky.moran@gmail.com>
2025-01-03 19:18:58 -0500

    darkmode final fix of theme.js and chakraProvider

--- Code-Editor-React/src/main.jsx
+++ Code-Editor-React/src/main.jsx
@@ -2,11 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { ChakraProvider } from '@chakra-ui/react'
-// import theme from './theme.js'
+import theme from './theme.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {localStorage.setItem('chakra-ui-color-mode', 'dark')} 
     <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
--- Code-Editor-React/src/theme.js
+++ Code-Editor-React/src/theme.js
@@ -1,12 +1,8 @@
-// currently the below code does not work for forcing dark mode on chakra-ui this is a known issue that has not been resolved
-// in the meantime dark mode achieved through local storage change
-// see main.jsx @ line 9
+import { extendTheme } from  '@chakra-ui/react';
 
-// import { extendTheme } from  '@chakra-ui/react';
+const theme = extendTheme({
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+});
 
-// const theme = extendTheme({
-//     initialColorMode: 'dark',
-//     useSystemColorMode: false,
-// });
-
-// export default theme;
+export default theme;
