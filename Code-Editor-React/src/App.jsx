b975b58b49520a8c36b7491fa9ff42f93b8da8eb
moran <sharon.ky.moran@gmail.com>
2025-01-03 20:17:45 -0500

    install monaco-editor for react

--- Code-Editor-React/src/App.jsx
+++ Code-Editor-React/src/App.jsx
@@ -1,10 +1,10 @@
-import { Box } from '@chakra-ui/react'
+import { Box, color } from '@chakra-ui/react'
 
 function App() {
 
   return (
-    <Box>
-      Hello hello
+    <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
+      Code Editor React
     </Box>
   )
 }
