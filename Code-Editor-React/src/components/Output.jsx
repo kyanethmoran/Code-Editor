66d9f30316f2e7d03ec97fd1dd85884bc0f8d326
moran <sharon.ky.moran@gmail.com>
2025-01-06 15:57:54 -0500

    add code output section and run code button

---
+++ Code-Editor-React/src/components/Output.jsx
@@ -0,0 +1,28 @@
+import { Box, Button, Text } from "@chakra-ui/react";
+import React from "react";
+
+const Output = () => {
+  const runCode = async () => {};
+
+  return (
+    <Box w="50%">
+      <Text mb={2} fontSize="lg">
+        Output
+      </Text>
+      <Button variant="outline" colorScheme="green" mb={4}>
+        Run Code
+      </Button>
+      <Box
+        height={"75vh"}
+        p={2}
+        border="1px solid"
+        borderRadius={4}
+        borderColor="#333"
+      >
+        Test
+      </Box>
+    </Box>
+  );
+};
+
+export default Output;
--- Code-Editor-React/src/components/codeEditor.jsx
+++ Code-Editor-React/src/components/codeEditor.jsx
@@ -1,8 +1,9 @@
-import { Box } from "@chakra-ui/react";
+import { Box, HStack } from "@chakra-ui/react";
 import { Editor } from "@monaco-editor/react";
 import { useRef, useState } from "react";
 import LanguageSelector from "./LanguageSelector";
 import { CODE_SNIPPETS } from "../Constants";
+import Output from "./Output";
 
 function CodeEditor() {
   const editorRef = useRef();
@@ -28,17 +29,22 @@ function CodeEditor() {
 
   return (
     <Box>
-      {/* pass language state as prop to the selector menu */}
-      <LanguageSelector language={language} onSelect={onSelect} />
-      <Editor
-        height="75vh"
-        theme="vs-dark"
-        language={language}
-        defaultValue={CODE_SNIPPETS[language]}
-        onMount={onMount}
-        value={value}
-        onChange={handleChange}
-      />
+      <HStack spacing={4}>
+        <Box w="50%">
+          {/* pass language state as prop to the selector menu */}
+          <LanguageSelector language={language} onSelect={onSelect} />
+          <Editor
+            height="75vh"
+            theme="vs-dark"
+            language={language}
+            defaultValue={CODE_SNIPPETS[language]}
+            onMount={onMount}
+            value={value}
+            onChange={handleChange}
+          />
+        </Box>
+        <Output />
+      </HStack>
     </Box>
   );
 }
