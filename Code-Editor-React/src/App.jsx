6998a23d0ef162a035ec721441bc3ae87109b626
moran <sharon.ky.moran@gmail.com>
2025-01-08 00:43:38 -0500

    move header

--- Code-Editor-React/src/App.jsx
+++ Code-Editor-React/src/App.jsx
@@ -1,11 +1,23 @@
 import { Box } from "@chakra-ui/react";
 import CodeEditor from "./components/codeEditor";
 import { Header } from "./components/Header";
+import { Flex, Switch } from "@chakra-ui/react";
 
 function App() {
   return (
     <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
-      <Header />
+      <Flex direction="column">
+        <Flex
+          justify="space-between"
+          border="2px solid"
+          padding={4}
+          marginBottom={4}
+          borderRadius={4}
+        >
+          <div>Code Editor React</div>
+          <Switch></Switch>
+        </Flex>
+      </Flex>
       <CodeEditor />
     </Box>
   );
