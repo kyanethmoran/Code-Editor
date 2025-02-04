b9480abccd8eab514803a08e691b53ceb31bca3c
moran <sharon.ky.moran@gmail.com>
2025-02-03 19:46:50 -0500

    theme

--- Code-Editor-React/src/main.jsx
+++ Code-Editor-React/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { ChakraProvider } from "@chakra-ui/react";
-import theme from "./theme.js";
+import theme from "./darkTheme.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
