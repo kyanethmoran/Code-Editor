611275474fd765a86f08cd4772455e9577aa4ab4
moran <sharon.ky.moran@gmail.com>
2025-02-14 16:58:43 -0500

    additional pages to router

--- Code-Editor-React/src/main.jsx
+++ Code-Editor-React/src/main.jsx
@@ -3,11 +3,27 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/home",
+    element: <App />,
+  },
+  {
+    path: "/SignUp",
+    element: <SignupPage />,
+  },
+  {
+    path: "/Login",
+    element: <LoginPage />,
   },
 ]);
 
